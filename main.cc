b701faf156b493e84638da0e378f55aa1cb14d0a
Szymon Sobociński <szymon.sobocinski@gmail.com>
2016-01-25 20:13:11 +0100

    Hello world N times

--- main.cc
+++ main.cc
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "config.h"
 
 int main()
 {
-	std::cout << "Hello world!" << std::endl;
+	for(std::size_t i=0; i<COUNT; ++i)
+		std::cout << "Hello world!" << std::endl;
 	return 0;
 }
